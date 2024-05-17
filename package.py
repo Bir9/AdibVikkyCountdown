2eb4b4b72f32747abc822dc54a53ae6015b7a2a9
Vikky Mutchukota <vmutchukota@gmail.com>
2024-05-17 16:29:22 -0400

    completed timeDifference function (no leap years)

--- package.py
+++ package.py
@@ -1,16 +1,13 @@
 monthDict = {
-    "Jan" : 1,
-    "Feb" : 2,
-    "Mar" : 3,
-    "Apr" : 4,
-    "May" : 5,
-    "Jun" : 6,
-    "Jul" : 7,
-    "Aug" : 8,
-    "Sep" : 9,
-    "Oct" : 10,
-    "Nov" : 11,
-    "Dec" : 12
+    "Jan" : 1, "Feb" : 2, "Mar" : 3, "Apr" : 4,
+    "May" : 5, "Jun" : 6, "Jul" : 7, "Aug" : 8,
+    "Sep" : 9, "Oct" : 10, "Nov" : 11, "Dec" : 12
+}
+
+daysInMonthDict = {
+    "1": 31, "2": 28, "3": 31, "4": 30,
+    "5": 31, "6": 30, "7": 31, "8": 31,
+    "9": 30, "10": 31, "11": 30, "12": 31
 }
 
 def timeDifference(start, end):
@@ -28,10 +25,58 @@ def timeDifference(start, end):
     endTime = endSplit[0].split(':')
     endDate = endSplit[1].split(' ')
     
-    print(startSplit, startTime, startDate)
-    print(endSplit, endTime, endDate)
+    years = int(endSplit[-1]) - int(startSplit[-1])
+    months = monthDict[endDate[0]] - monthDict[startDate[0]]
+    days = 0
+    
+    if years >= 0:
+        if months < 0:
+            if years > 0:
+                years -= 1
+                months += 12
+            else:
+                return print("user input error")
+            
+    for i in range(months):
+        j = i + monthDict[startDate[0]]
+        days += daysInMonthDict[str(j%12)]
+    
+    days += int(endDate[1]) - int(startDate[1])
+    hours = int(endTime[0]) - int(startTime[0])
+    minutes = int(endTime[1]) - int(startTime[1])
+    seconds = int(endTime[2]) - int(startTime[2])
+    
+    if years >= 0:
+        if days < 0:
+            if years > 0:
+                years -= 1
+                days += 365
+            else:
+                return print("user input error")
+        if hours < 0:
+            if days > 0:
+                days -= 1
+                hours += 24
+            else:
+                return print("user input error")
+        if minutes < 0:
+            if hours > 0:
+                hours -= 1
+                minutes += 60
+            else:
+                return print("user input error")
+        if seconds < 0:
+            if minutes > 0:
+                minutes -= 1
+                seconds += 60
+            else:
+                return print("user input error")
+    else:
+        return print("user input error")
+    
+    print(f"{years} years {days} days {hours} hours {minutes} minutes {seconds} seconds left")
 
-timeDifference("09:52:14, Apr 04, 2022", "12:32:59, Jan 20, 2025")
+timeDifference( "09:52:14, May 04, 2022", "14:20:00, Jun 27, 2022")
 
 def checkLeapYear(year):
     if year % 4 == 0:
